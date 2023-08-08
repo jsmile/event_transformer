b8ed59549281372bf4a7a7f2b5d04d2e1479af20
Byeong Gui Jeong <jsmile@naver.com>
2023-08-08 19:34:51 +0900

    modify counter bloc for event transformer

--- lib/blocs/counter/counter_bloc.dart
+++ lib/blocs/counter/counter_bloc.dart
@@ -6,12 +6,18 @@ part 'counter_state.dart';
 
 class CounterBloc extends Bloc<CounterEvent, CounterState> {
   CounterBloc() : super(CounterState.initial()) {
-    on<CounterIncreasedEvent>((event, emit) {
-      emit(state.copyWith(counter: state.counter + 1));
-    });
+    on<CounterIncreasedEvent>(_counterIncreasedEvent);
 
-    on<CounterDecreasedEvent>((event, emit) {
-      emit(state.copyWith(counter: state.counter - 1));
-    });
+    on<CounterDecreasedEvent>(_counterDecreasedEvent);
+  }
+
+  Future<void> _counterIncreasedEvent(event, emit) async {
+    await Future.delayed(const Duration(seconds: 4));
+    emit(state.copyWith(counter: state.counter + 1));
+  }
+
+  Future<void> _counterDecreasedEvent(event, emit) async {
+    await Future.delayed(const Duration(seconds: 2));
+    emit(state.copyWith(counter: state.counter - 1));
   }
 }
